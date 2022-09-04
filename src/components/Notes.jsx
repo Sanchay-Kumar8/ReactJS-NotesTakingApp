b671265642894c066c2bea65f48da543a069db73
Sanchay-Kumar8 <71579398+Sanchay-Kumar8@users.noreply.github.com>
2022-09-04 19:30:39 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is Heading for notes</h1>
+      <p>This is test text for the paragraph tag</p>
+    </div>
+  );
+}
+
+export default Notes;
